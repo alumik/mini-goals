ec55bdd082c67b37301bf5d2f4d31c211708d815
Zhong Zhenyu <nczzy1997@gmail.com>
2019-06-05 11:42:02 +0800

    完成微信用户登录

--- app.js
+++ app.js
@@ -1,39 +1,55 @@
 //app.js
+
+const Fly = require('miniprogram_npm/flyio/index')
+const fly = new Fly()
+const server_url = 'http://localhost/api/v1/'
+
 App({
-  onLaunch: function () {
-    // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
+    onLaunch: function () {
+        wx.login({
             success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
-
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
+                fly.post(this.globalData.server_url.user, {
+                    code: res.code
+                }).then(response => {
+                    this.globalData.openid = response.data.openid
+                }).catch(err => {
+                    console.log(err)
+                })
             }
-          })
+        })
+
+        wx.getSetting({
+            success: res => {
+                if (res.authSetting['scope.userInfo']) {
+                    wx.getUserInfo({
+                        success: res => {
+                            this.globalData.userInfo = res.userInfo
+
+                            if (this.userInfoReadyCallback) {
+                                this.userInfoReadyCallback(res)
+                            }
+
+                            fly.put(this.globalData.server_url.user, {
+                                openid: this.globalData.openid,
+                                content: {
+                                    name: res.userInfo.nickName,
+                                    avatar: res.userInfo.avatarUrl
+                                }
+                            }).catch(err => {
+                                console.log(err)
+                            })
+                        }
+                    })
+                }
+            }
+        })
+    },
+
+    globalData: {
+        userInfo: null,
+        openid: null,
+        server_url: {
+            user: server_url + 'user'
         }
-      }
-    })
-  },
-  globalData: {
-    userInfo: null
-  }
+    }
 })
